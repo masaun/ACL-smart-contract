0e6fdb68d9d28a2f0e8401d9f397e9079f81bf89
masaun <masanoriuno@gmail.com>
2022-02-23 11:51:24 -0700

    [SC]: Define the AccessControlList.sol

---
+++ contracts/AccessControlList.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract AccessControlList {
+
+    constructor() {}
+
+    function get() public view returns (string memory) {}
+
+    function set() public {}
+
+}
