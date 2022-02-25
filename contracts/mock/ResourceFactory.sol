3cc52847217542f061f0082d12693e197469564a
masaun <masanoriuno@gmail.com>
2022-02-25 07:14:55 -0700

    [SC]: Define getResource() in ResourceFactory.sol

--- contracts/mock/ResourceFactory.sol
+++ contracts/mock/ResourceFactory.sol
@@ -5,6 +5,11 @@ import { Resource } from "./Resource.sol";
 
 import "hardhat/console.sol";
 
+
+/**
+ * @dev - 
+ * @dev - On the assumption that, each resource has resource ID and own contract address.
+ */ 
 contract ResourceFactory {
 
     uint currentResourceId;  //@dev - Count of resource ID is started from 0
@@ -17,6 +22,18 @@ contract ResourceFactory {
         Resource resource = new Resource();
         resourceAddresses.push(address(resource));
         currentResourceId++;
+    }
+
+    //-----------------
+    // Getter methods
+    //-----------------
+
+    /**
+     * @dev - A resource is identified by a resourceId (uint256)
+     * @return resourceAddress - Resource's contract address that is associated with resource ID specified
+     */ 
+    function getResource(uint resourceId) public returns (address resourceAddress) {
+        return resourceAddresses[resourceId];
     }
 
 }
